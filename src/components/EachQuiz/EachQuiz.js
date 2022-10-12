4729e147cc03c6bca7b7f5155daa1a3931a4cfb0
siamshahriar <siamshahriar99@gmail.com>
2022-10-12 10:43:17 +0600

    dynamic button created with id

--- src/components/EachQuiz/EachQuiz.js
+++ src/components/EachQuiz/EachQuiz.js
@@ -5,13 +5,13 @@ import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
 import './EachQuiz.css';
 
 const EachQuiz = ({quiz}) => {
-    const {logo, name} = quiz;
+    const {logo, id, name} = quiz;
     return (
         <div className='eachQuiz'>
             <img src={logo} alt="" />
             <div className='eachQuiz-details'>
                 <span>{name}</span>
-                <Link>Start Practice<FontAwesomeIcon className='directionIcon' icon={faArrowRight} /></Link>
+                <Link to={`/details/${id}`}>Start Practice<FontAwesomeIcon className='directionIcon' icon={faArrowRight} /></Link>
             </div>
         </div>
     );
